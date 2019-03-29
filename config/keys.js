4b6f6a41002df73b2a65a684b3463db86c2e51c6
Waldo Rabie <waldo.rabie@smartdata.net>
2019-03-29 00:49:45 -0400

    Add authentication

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://admin:admin@cluster0-1afs7.mongodb.net/test?retryWrites=true'
-}
--- routes/api/items.js
+++ routes/api/items.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 // Item Model
 const Item = require('../../models/Item');
@@ -8,30 +9,29 @@ const Item = require('../../models/Item');
 // @desc    Get ALL Items
 // @access  Public
 router.get('/', (req, res) => {
-    Item.find()
-        .sort({ date: -1})
-        .then(items => res.json(items))
+	Item.find()
+		.sort({ date: -1 })
+		.then(items => res.json(items));
 });
 
 // @route   POST api/items
 // @desc    Create an Item
-// @access  Public
-router.post('/', (req, res) => {
-    const newItem = new Item({
-        name: req.body.name
-    });
+// @access  Private
+router.post('/', auth, (req, res) => {
+	const newItem = new Item({
+		name: req.body.name
+	});
 
-    newItem.save().then(item => res.json(item));
+	newItem.save().then(item => res.json(item));
 });
 
 // @route   DELETE api/items/:id
 // @desc    Delete an Item
-// @access  Public
-router.delete('/:id', (req, res) => {
-    Item.findById(req.params.id)
-        .then(item => item.remove().then(() => res.json({ success: true })))
-        .catch(err => res.status(404).json({ success: false }));
-
-})
+// @access  Private
+router.delete('/:id', auth, (req, res) => {
+	Item.findById(req.params.id)
+		.then(item => item.remove().then(() => res.json({ success: true })))
+		.catch(err => res.status(404).json({ success: false }));
+});
 
 module.exports = router;
